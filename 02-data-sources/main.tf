ca74d71a594a07ffbbad23d99d5a709c19e24868
Bindu P <bindupilli1314@gmail.com>
2023-04-30 15:20:45 +0530

    terraform

--- 02-data-sources/main.tf
+++ 02-data-sources/main.tf
@@ -1,9 +1,13 @@
-data "aws_security_group" "selected" {
-  name = "allow-all"
+//data "aws_security_group" "selected" {
+//  name = "allow-all"
+//}
+
+//output "security_group_id" {
+//  value = data.aws_security_group.selected.id
+//}
+
+data "aws_security_groups" "test" {}
+
+output "all_sg" {
+  value = data.aws_security_groups.test
 }
-
-output "security_group_id" {
-  value = data.aws_security_group.selected
-}
-
-
