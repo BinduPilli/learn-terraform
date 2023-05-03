0a79751a86c1965e34f970c0267738b623f5d0e8
Bindu P <bindupilli1314@gmail.com>
2023-05-03 15:49:41 +0530

    terraform

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -1,20 +1,29 @@
 
-resource "null_resource" "fruits" {
-  //count = length(var.fruits)
+resource "null_resource" "fruits1" {
 
+  for_each = var.fruits1
 
- provisioner "local-exec" {
-  //command = "echo fruit name - ${var.fruits[count.index]}"
-  command = "echo ${length(var.fruits)}"
- }
+  provisioner "local-exec" {
+    command = "echo fruit name - ${each.key} - ${each.value["count"]}"
+    //command = "echo ${length(var.fruits)}"
+  }
 
 }
 
-variable "fruits" {
+variable "fruits1" {
   default = {
-    apple = 10
-    orange = 200
-    banana = 100
+    apple = {
+      name = "apple"
+      count = 10
+    }
+    orange = {
+      name = "orange"
+      count = 200
+    }
+    banana = {
+      name = "banana"
+      count = 100
+    }
   }
 }
 
